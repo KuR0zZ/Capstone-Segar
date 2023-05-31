208d5d73f7f5b6af7d5457d1a354381a4812da00
KuR0zZ <ferdinandiphone6@gmail.com>
2023-05-31 15:59:52 +0700

    Add hostname and change port to 8080

--- src/app.js
+++ src/app.js
@@ -11,12 +11,13 @@ app.use(express.json())
 // Routes
 app.use('/api/v1/vegetables', router)
 
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 8080
+const HOST = '0.0.0.0'
 
 const startServer = async () => {
   try {
     await connectDB(process.env.MONGO_URI)
-    app.listen(PORT, console.log(`Server is listening on port ${PORT}`))
+    app.listen(PORT, HOST, console.log(`Server is listening on port ${PORT}`))
   } catch (error) {
     console.log(error)
   }
