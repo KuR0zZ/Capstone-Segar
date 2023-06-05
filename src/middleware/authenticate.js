38edf36a775355901480c1a76dffa5de148f2c04
Calvin Danyalson <calvindany1102@gmail.com>
2023-06-06 01:01:42 +0700

    Adding token to User model and use it for additional authentication

--- src/middleware/authenticate.js
+++ src/middleware/authenticate.js
@@ -2,10 +2,11 @@ const passport = require("passport");
 
 const authenticate = (req, res, next) => {
   passport.authenticate("jwt", (err, user, info) => {
+    const token = req.headers.authorization.split(' ')[1];
     if (err) {
       return res.status(500).json({error: true, message: "Something went wrong" });
     }
-    if (!user) {
+    if (!user || user.token != token) {
       return res.status(401).json({error: false, message: "Authentication failed" });
     }
     next();
